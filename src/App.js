11278cdb965ef2827771c4e05a4a99edcc0fc6ef
Olatunde <idowuolatunde32@gmail.com>
2022-08-01 14:30:24 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './App.css';
 import { Header } from './components/Header';
 import {Balance } from './components/Balance';
 import {IncomeExpenses} from './components/IncomeExpenses';
 import {TransactionList} from './components/TransactionList';
 import {AddTransaction} from './components/AddTransaction';
 
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
  
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className='container'>
         <Balance />
@@ -17,7 +20,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -35,4 +35,4 @@ export const GlobalProvider = ({ children }) => {
   }}>
     {children}
   </GlobalContext.Provider>);
-}
+} 
